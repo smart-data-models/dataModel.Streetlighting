18179689c7e64bad28e9e8928bb4dca58e792188
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:22:26 +0200

    created example of code

--- StreetlightControlCabinet/code/code_for_using_dataModel.Streetlighting_StreetlightControlCabinet.py
+++ StreetlightControlCabinet/code/code_for_using_dataModel.Streetlighting_StreetlightControlCabinet.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "StreetlightControlCabinet"
 subject = "dataModel.Streetlighting"
-brandName = "{'type': 'Property', 'value': 'Siemens'}"
+brandName = "Siemens"
 attribute = "brandName"
 value = brandName
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-compliantWith = {'type': 'Property', 'value': ['IP54']}
+compliantWith = ['IP54']
 attribute = "compliantWith"
 value = compliantWith
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-cupboardMadeOf = "{'type': 'Property', 'value': 'plastic'}"
+cupboardMadeOf = "plastic"
 attribute = "cupboardMadeOf"
 value = cupboardMadeOf
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-dateLastProgramming = "{'type': 'Property', 'value': {'@type': 'DateTime', '@value': '2016-07-08T16:04:30.201Z'}}"
+dateLastProgramming = "2016-07-08T16:04:30.201Z"
 attribute = "dateLastProgramming"
 value = dateLastProgramming
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
